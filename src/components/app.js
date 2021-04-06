df17b5efc753f15b7c5d77a3f65bf30590e98dd6
Alexandre <alonsocabado@hotmail.com>
2021-04-06 09:56:00 +0200

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Focazul extends Component {
+  render() {
+    return (
+      <div className="focazul">
+        <h1>Focazul</h1>
+      </div>
+    );
+  }
+}
