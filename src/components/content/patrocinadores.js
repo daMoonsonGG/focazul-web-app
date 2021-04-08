c1a13fd799aae183921b19531f525bd98032a256
Alexandre <alonsocabado@hotmail.com>
2021-04-08 13:30:03 +0200

    Finished patrocinadores component

--- src/components/content/patrocinadores.js
+++ src/components/content/patrocinadores.js
@@ -2,7 +2,101 @@ import React, { Component } from "react";
 
 class Patrocinadores extends Component {
   render() {
-    return <div>ihflidshflidsf</div>;
+    return (
+      <div className="patrocinadores">
+        <div className="patrocinadores__title">Patrocinadores</div>
+        <div className="patrocinadores__comment">
+          Múltiples empresas y profesionales han colaborado y/o colaboran con la
+          misión de Asociación Focazul.
+          <br /> Desde aquí queremos agradecer todo el apoyo recibido e
+          invitaros a explorar su trabajo.
+          <br /> Si tienes un negocio y quieres ser partícipe, no dudes en
+          ponerte en{" "}
+          <a className="contacto" href="#">
+            contacto
+          </a>{" "}
+          con nosotros.
+        </div>
+        <div className="patrocinadores__patrocinadores">
+          <div className="patrocinadores__sponsored-patrocinador">
+            <a href="https://thebestsport.es/" target="_blank">
+              <img
+                className="sponsored-img"
+                src="https://i.imgur.com/mInulVS.png"
+              />
+            </a>
+            <div className="sponsored-code">Código: FOCAZUL19BO</div>
+            <div className="sponsored-description">
+              Un 5% de descuento y otro 5% destinado a la Asociación.
+            </div>
+          </div>
+          <div className="patrocinadores__grupo">
+            <div className="patrocinadores__patrocinador">
+              <img
+                className="patrocinador-img"
+                src="https://i.imgur.com/1h1Snmk.jpg"
+              />
+            </div>
+            <div className="patrocinadores__patrocinador">
+              <a href="https://vypgestion.es/" target="_blank">
+                <img
+                  className="patrocinador-img"
+                  src="https://i.imgur.com/fAaVaQH.png"
+                />
+              </a>
+            </div>
+            <div className="patrocinadores__patrocinador">
+              <img
+                className="patrocinador-img"
+                src="https://i.imgur.com/1h1Snmk.jpg"
+              />
+            </div>
+            <div className="patrocinadores__patrocinador">
+              <img
+                className="patrocinador-img"
+                src="https://i.imgur.com/fAaVaQH.png"
+              />
+            </div>
+            <div className="patrocinadores__patrocinador">
+              <img
+                className="patrocinador-img"
+                src="https://i.imgur.com/1h1Snmk.jpg"
+              />
+            </div>
+            <div className="patrocinadores__patrocinador">
+              <img
+                className="patrocinador-img"
+                src="https://i.imgur.com/fAaVaQH.png"
+              />
+            </div>
+            <div className="patrocinadores__patrocinador">
+              <img
+                className="patrocinador-img"
+                src="https://i.imgur.com/1h1Snmk.jpg"
+              />
+            </div>
+            <div className="patrocinadores__patrocinador">
+              <img
+                className="patrocinador-img"
+                src="https://i.imgur.com/fAaVaQH.png"
+              />
+            </div>
+            <div className="patrocinadores__patrocinador">
+              <img
+                className="patrocinador-img"
+                src="https://i.imgur.com/1h1Snmk.jpg"
+              />
+            </div>
+            <div className="patrocinadores__patrocinador">
+              <img
+                className="patrocinador-img"
+                src="https://i.imgur.com/fAaVaQH.png"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
